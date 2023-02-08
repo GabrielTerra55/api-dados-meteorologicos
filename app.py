facc2bff0e75d0a9dbaad68a11a06baf9f32b32d
Gabriel Terra <gabrielterra055@gmail.com>
2023-02-08 16:55:19 -0300

    tratamento de erro de digitaçao

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import requests
 app = Flask(__name__)
 
 info = {'atencao': 'E necessario que voce insira o nome da cidade que deseja'}
+erro = {'atencao': 'Pode ter ocorrido um erro de digitacao com o municipio desejado! tente novamente',
+        'observacao': 'Se o municipio escolhido foi Sao Paulo, ignore o aviso'
+}
+
 
 
 @app.route('/time', methods=['GET'])
@@ -13,9 +17,9 @@ def info_inicial():
 
 @app.route('/time/<name>', methods=['GET'])
 def obter_dados_meteorologicos(name):
-    cidade = name
+    nome_cidade = name
     data = requests.get(
-        'https://api.hgbrasil.com/weather?format=json&key=SUA-CHAVE&city_name={}'.format(cidade))
+        'https://api.hgbrasil.com/weather?format=json&key=SUA-CHAVE&city_name={}'.format(nome_cidade))
 
     data = data.json()
     data = data['results']
@@ -25,8 +29,12 @@ def obter_dados_meteorologicos(name):
               'rain': data.get('rain'),
               'humidity': data.get('humidity')
               }
+    print(data['city'])
+   
+    if cidade['city'] != 'São Paulo, SP':
+        return jsonify(cidade)
 
-    return jsonify(cidade)
+    return jsonify(cidade, erro)
 
 if __name__ == '__main__':
     app.run()
