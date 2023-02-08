a3d83d319a666db0ce9db569de84c969e19f6cc0
Gabriel Terra <gabrielterra055@gmail.com>
2023-02-08 08:06:13 -0300

    response em json

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify, request
-# import requests
+from flask import Flask, jsonify
+import requests
 
 app = Flask(__name__)
 
@@ -14,48 +14,21 @@ def info_inicial():
 @app.route('/time/<name>', methods=['GET'])
 def obter_dados_meteorologicos(name):
     cidade = name
-    return "a cidade é {}".format(cidade)
     data = requests.get(
-        'https://api.hgbrasil.com/weather?key=SUA-CHAVE&city_name=Campinas,SP')
+        'https://api.hgbrasil.com/weather?key=SUA-CHAVE&city_name={}'.format(cidade))
+
+    print(type(data))
+    data = data.json()
+    data = data['results']
+    print(data.get("city", 0))
+    cidade = {'city': data.get('city'),
+              'temp': data.get('temp'),
+              'rain': data.get('rain'),
+              'humidity': data.get('humidity')
+              }
+    print(cidade)
+
+    return jsonify(cidade)
 
 
 app.run(port=5000, host='localhost', debug=True)
-
-
-'''
-# Consultar(id)
-@app.route('/livros/<int:id>',methods=['GET'])
-def obter_livro_por_id(id):
-    for livro in livros:
-        if livro.get('id') == id:
-            return jsonify(livro)
-# Editar
-@app.route('/livros/<int:id>',methods=['PUT'])
-def editar_livro_por_id(id):
-    livro_alterado = request.get_json()
-    for indice,livro in enumerate(livros):
-        if livro.get('id') == id:
-            livros[indice].update(livro_alterado)
-            return jsonify(livros[indice])
-# Criar
-@app.route('/livros',methods=['POST'])
-def incluir_novo_livro():
-    novo_livro = request.get_json()
-    livros.append(novo_livro)
-    
-    return jsonify(livros)
-# Excluir
-@app.route('/livros/<int:id>',methods=['DELETE'])
-def excluir_livro(id):
-    for indice, livro in enumerate(livros):
-        if livro.get('id') == id:
-            del livros[indice]
-
-    return jsonify(livros)
-
-'''
-
-'''@app.route("/", methods=["GET"])
-def index():
-	data = requests.get('https://randomuser.me/api')
-	return data.json()'''
